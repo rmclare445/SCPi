ef2a2d6c95c9c3a8f0f9e2a8d1719e5f55251f67
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-06-10 01:44:14 -0700

    Add keys

--- SCPi/scpi.py
+++ SCPi/scpi.py
@@ -1,5 +1,6 @@
 import paramiko
 from scp import SCPClient
+import info.keys as keys
 
 class Connection():
     
@@ -8,8 +9,12 @@ class Connection():
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.load_system_host_keys()
-        self.ssh.connect('hostname', username='user', password='password')
+        self.ssh.connect(keys.headboard.host, username=keys.headboard.user, 
+                         password=keys.headboard.passwd)
         
     def send(self, file):
         scp = SCPClient(self.ssh.get_transport())
         scp.put(file)
+
+#g = Connection()
+#g.send(b'C:\Users\Ryan\Documents\message.txt')
